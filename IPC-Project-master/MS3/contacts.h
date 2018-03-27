dd70e33eab7412695dd85fdd12823485f4627bf7
tjudarbe <35641474+tjudarbe@users.noreply.github.com>
2018-03-26 22:47:49 -0400

    Add files via upload

---
+++ IPC-Project-master/MS3/contacts.h
@@ -0,0 +1,50 @@
+/* -------------------------------------------
+Name:
+Student number:
+Email:
+Section:
+Date:
+----------------------------------------------
+Assignment: 1
+Milestone:  3
+---------------------------------------------- */
+
+// Structure type Name declaration
+struct Name {
+    char firstName[31];
+    char middleInitial[7];
+    char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code from Milestone #2 here...
+
+struct Address {
+   int streetNumber;
+   char street[41];
+   int apartmentNumber;
+   char postalCode[8];
+   char city[41];
+
+};
+
+
+// Structure type Numbers declaration
+// Place your code from Milestone #2 here...
+
+struct Numbers {
+   char cell[21];
+   char home[21];
+   char business[21];
+};
+
+
+// Structure type Contact declaration
+// Place your code here...
+
+struct Contact {
+   typedef Name name;
+   typedef Address address;
+   typedef Numbers numbers;
+
+};
